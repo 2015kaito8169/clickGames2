4cc6ef46d6e9326a4441b9b01cacc8802a83fec8
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-02-09 14:01:10 +0900

    Update ClickGame.cs

--- Assets/ClickGame.cs
+++ Assets/ClickGame.cs
@@ -1,61 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ClickGame : MonoBehaviour {
     private int s = 0;
-    private int h = 0;
-    private float t = 10f;
-    private bool a = false;
-    private Text u;
-
+    private Text t;
     void Start() {
-        h = PlayerPrefs.GetInt("H", 0);
-        GameObject cO = new GameObject("C");
-        Canvas c = cO.AddComponent<Canvas>();
-        c.renderMode = RenderMode.ScreenSpaceOverlay;
-        cO.AddComponent<CanvasScaler>();
-        cO.AddComponent<GraphicRaycaster>();
-
-        GameObject bO = new GameObject("B");
-        bO.transform.SetParent(cO.transform);
-        Button b = bO.AddComponent<Button>();
-        b.gameObject.AddComponent<Image>().rectTransform.sizeDelta = new Vector2(400, 400);
-
+        GameObject c = new GameObject("C");
+        c.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        c.gameObject.AddComponent<GraphicRaycaster>();
+        GameObject b = new GameObject("B");
+        b.transform.SetParent(c.transform);
+        b.AddComponent<Button>().onClick.AddListener(() => { s++; t.text = "SCORE: " + s; });
+        b.AddComponent<Image>().rectTransform.sizeDelta = new Vector2(500, 500);
         GameObject tO = new GameObject("T");
-        tO.transform.SetParent(cO.transform);
-        u = tO.AddComponent<Text>();
-        u.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-        u.fontSize = 40;
-        u.alignment = TextAnchor.MiddleCenter;
-        u.rectTransform.localPosition = new Vector3(0, 300, 0);
-        u.color = Color.white;
-        u.text = "BEST: " + h + " / TIME: 10 / SCORE: 0";
-
-        b.onClick.AddListener(() => {
-            if (t > 0) {
-                a = true;
-                s++;
-            }
-        });
-    }
-
-    void Update() {
-        if (a && t > 0) {
-            t -= Time.deltaTime;
-            u.text = "TIME: " + Mathf.Ceil(t) + " SCORE: " + s;
-            if (t <= 0) {
-                string r = "NOOB";
-                if (s >= 100) { r = "GOD"; }
-                else if (s >= 70) { r = "PRO"; }
-                else if (s >= 40) { r = "AMATEUR"; }
-
-                if (s > h) {
-                    h = s;
-                    PlayerPrefs.SetInt("H", h);
-                    PlayerPrefs.Save();
-                }
-                u.text = "RANK: " + r + " / SCORE: " + s + " / BEST: " + h;
-            }
-        }
+        tO.transform.SetParent(c.transform);
+        t = tO.AddComponent<Text>();
+        t.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        t.fontSize = 100;
+        t.alignment = TextAnchor.MiddleCenter;
+        t.text = "SCORE: 0";
     }
 }
