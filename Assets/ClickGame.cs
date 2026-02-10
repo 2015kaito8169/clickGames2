3a60de0410ffce91a580f5e195f1b528c043253a
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-02-10 13:34:41 +0900

    Update ClickGame.cs

--- Assets/ClickGame.cs
+++ Assets/ClickGame.cs
@@ -6,33 +6,39 @@ public class ClickGame : MonoBehaviour {
     private Text u;
 
     void Start() {
-        
         GameObject cO = new GameObject("C");
         Canvas canvas = cO.AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        cO.AddComponent<CanvasScaler>();
-        cO.AddComponent<GraphicRaycaster>();
-
-
-    　　 GameObject bO = new GameObject("B");
-        bO.transform.SetParent(cO.transform);
-        Button b = bO.AddComponent<Button>();
-        b.gameObject.AddComponent<Image>().rectTransform.sizeDelta = new Vector2(400, 400);
-
+        cO.AddComponent<CanvasScaler>(); // 画面解像度への自動対応
+        cO.AddComponent<GraphicRaycaster>(); // クリック反応に必須
+　
+        GameObject bO = new GameObject("B");
+        bO.transform.SetParent(cO.transform, false); 
+        bO.AddComponent<CanvasRenderer>(); 
         
-        GameObject o = new GameObject("T");
-        co.transform.SetParent(cO.transform); 
-        u. = o.AddComponent<Text>();
+        Image i = bO.AddComponent<Image>();
+        i.rectTransform.sizeDelta = new Vector2(400, 400);
+        
+        Button b = bO.AddComponent<Button>();
+        b.onClick.AddListener(() => {
+            s++;
+            if (u != null) u.text = "S:" + s;
+        });
+
+       
+        GameObject tO = new GameObject("T");
+        tO.transform.SetParent(cO.transform, false);
+        tO.AddComponent<CanvasRenderer>(); // 描画に必須
+        
+        u = tO.AddComponent<Text>();
+   
         u.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
         u.fontSize = 80;
         u.alignment = TextAnchor.MiddleCenter;
         u.color = Color.white;
         u.text = "S:0";
 
-        
-        b.onClick.AddListener(() => {
-            s++;
-            if (t != null) u.text = "S:" + s;
-        });
+        u.rectTransform.sizeDelta = new Vector2(600, 200);
+        u.rectTransform.localPosition = new Vector3(0, 300, 0);
     }
 }
