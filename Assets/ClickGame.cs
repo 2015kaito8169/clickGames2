27be057e5a1ee39a59d271092a108f378572bc24
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-02-10 15:10:46 +0900

    Update ClickGame.cs

--- Assets/ClickGame.cs
+++ Assets/ClickGame.cs
@@ -6,21 +6,16 @@ public class ClickGame : MonoBehaviour {
     private Text u;
 
     void Start() {
-     GameObject cO = new GameObject("C");
+        GameObject cO = new GameObject("C");
         Canvas canvas = cO.AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        cO.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize; // 追記：解像度固定
+        cO.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
         cO.AddComponent<GraphicRaycaster>();
 
-        
         GameObject bO = new GameObject("B");
         bO.transform.SetParent(cO.transform, false);
         bO.AddComponent<CanvasRenderer>();
-        
-        Image i = bO.AddComponent<Image>();
-        i.rectTransform.sizeDelta = new Vector2(400, 400);
-        i.color = new Color(1, 1, 1, 0.5f); 
-        
+        bO.AddComponent<Image>().rectTransform.sizeDelta = new Vector2(400, 400);
         bO.AddComponent<Button>().onClick.AddListener(() => {
             s++;
             if (u != null) u.text = "S:" + s;
@@ -32,13 +27,12 @@ public class ClickGame : MonoBehaviour {
         
         u = tO.AddComponent<Text>();
         
-        u.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf"); 
-        if(u.font == null) u.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-
+        u.font = Font.CreateDynamicFontFromOSFont("Arial", 80); 
+        
         u.fontSize = 80;
         u.alignment = TextAnchor.MiddleCenter;
-        u.color = Color.black; 
-　      u.text = "S:0";
+        u.color = Color.white;
+        u.text = "S:0";
         u.rectTransform.sizeDelta = new Vector2(600, 200);
         u.rectTransform.localPosition = new Vector3(0, 300, 0);
     }
