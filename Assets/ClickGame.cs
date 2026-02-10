cbdf93695240ec4bfb6e65a99785b7b0c06c8506
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-02-10 14:50:11 +0900

    Update ClickGame.cs

--- Assets/ClickGame.cs
+++ Assets/ClickGame.cs
@@ -6,38 +6,39 @@ public class ClickGame : MonoBehaviour {
     private Text u;
 
     void Start() {
-        GameObject cO = new GameObject("C");
+     GameObject cO = new GameObject("C");
         Canvas canvas = cO.AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        cO.AddComponent<CanvasScaler>(); // 画面解像度への自動対応
-        cO.AddComponent<GraphicRaycaster>(); // クリック反応に必須
-　
+        cO.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize; // 追記：解像度固定
+        cO.AddComponent<GraphicRaycaster>();
+
+        
         GameObject bO = new GameObject("B");
-        bO.transform.SetParent(cO.transform, false); 
-        bO.AddComponent<CanvasRenderer>(); 
+        bO.transform.SetParent(cO.transform, false);
+        bO.AddComponent<CanvasRenderer>();
         
         Image i = bO.AddComponent<Image>();
         i.rectTransform.sizeDelta = new Vector2(400, 400);
+        i.color = new Color(1, 1, 1, 0.5f); 
         
-        Button b = bO.AddComponent<Button>();
-        b.onClick.AddListener(() => {
+        bO.AddComponent<Button>().onClick.AddListener(() => {
             s++;
             if (u != null) u.text = "S:" + s;
         });
 
-       
         GameObject tO = new GameObject("T");
         tO.transform.SetParent(cO.transform, false);
-        tO.AddComponent<CanvasRenderer>(); // 描画に必須
+        tO.AddComponent<CanvasRenderer>();
         
         u = tO.AddComponent<Text>();
-   
-        u.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        
+        u.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf"); 
+        if(u.font == null) u.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+
         u.fontSize = 80;
         u.alignment = TextAnchor.MiddleCenter;
-        u.color = Color.white;
-        u.text = "S:0";
-
+        u.color = Color.black; 
+　      u.text = "S:0";
         u.rectTransform.sizeDelta = new Vector2(600, 200);
         u.rectTransform.localPosition = new Vector3(0, 300, 0);
     }
